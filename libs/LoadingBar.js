a9be7d27f1bed10f887ab5e1e15b8ab387590b84
Nicholas Lever <nik.j.lever@gmail.com>
2022-10-18 08:43:24 +0100

    Updated LoadingBar

--- libs/LoadingBar.js
+++ libs/LoadingBar.js
@@ -48,6 +48,37 @@ class LoadingBar{
 			this.domElement.style.display = 'none';
 		}
 	}
+
+	get loaded(){
+		if ( this.assets === undefined ) return false;
+		
+		let ploaded=0, ptotal=0;
+		Object.values(this.assets).forEach( asset => {
+			ploaded += asset.loaded;
+			ptotal += asset.total;
+		});
+
+		return ploaded == ptotal;
+	}
+
+	update(assetName, loaded, total){
+		if ( this.assets === undefined ) this.assets = {};
+		
+		if ( this.assets[assetName] === undefined ){
+			this.assets[assetName] = { loaded, total };
+		}else{
+			this.assets[assetName].loaded = loaded;
+			this.assets[assetName].total = total;
+		}
+		
+		let ploaded=0, ptotal=0;
+		Object.values(this.assets).forEach( asset => {
+			ploaded += asset.loaded;
+			ptotal += asset.total;
+		});
+
+		this.progress = ploaded/ptotal;
+	}
 }
 
 export { LoadingBar };
