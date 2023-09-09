e0587f58a8665c66260e81647b3e772087c6e1fa
Nik Lever <47000827+NikLever@users.noreply.github.com>
2023-09-09 11:01:18 +0200

    Updated to v156

--- complete/src/app.js
+++ complete/src/app.js
@@ -30,7 +30,7 @@ class App{
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        this.renderer.outputColorSpace = THREE.SRGBColorSpace;
         
 		container.appendChild( this.renderer.domElement );
         
