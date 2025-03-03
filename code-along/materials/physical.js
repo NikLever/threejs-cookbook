9b988b932521c1ec3b869ed15d0540788fd639e6
Nicholas Lever <nik@catalystpics.co.uk>
2025-03-03 10:14:18 +0000

    Before refraction

---
+++ code-along/materials/physical.js
@@ -0,0 +1,142 @@
+import * as THREE from "three"
+    import { OrbitControls } from "three/addons/controls/OrbitControls.js"
+    import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+    import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+    import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+    import { GUI } from "three/addons/libs/lil-gui.module.min.js"
+
+    let camera, scene, renderer, gui, envMap, params, material;
+
+    init();
+
+    function init() {
+
+        const container = document.createElement( 'div' );
+        document.body.appendChild( container );
+
+        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
+        camera.position.set( 2.5, 1.5, 3.0 );
+
+        scene = new THREE.Scene();
+
+        const ambient = new THREE.HemisphereLight(0xffffbb, 0x080820);
+        scene.add(ambient);
+    
+        const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+        light.position.set( 1, 10, 6);
+        scene.add(light);
+
+        renderer = new THREE.WebGLRenderer( { antialias: true } );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.outputColorSpace = THREE.SRGBColorSpace;
+        container.appendChild( renderer.domElement );
+
+        const controls = new OrbitControls( camera, renderer.domElement );
+        controls.addEventListener( 'change', render ); // use if there is no animation loop
+        controls.minDistance = 2;
+        controls.maxDistance = 10;
+        controls.target.set( 0, 1.2, 0 );
+        controls.update();
+
+        window.addEventListener( 'resize', onWindowResize );
+
+        setEnvironment();
+        
+        createGUI();
+
+        loadModel();
+
+    }
+
+    function onWindowResize() {
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+        render();
+
+    }
+
+    function setEnvironment(){
+        new RGBELoader()
+            .setPath( '../../assets/hdr/' )
+            .load( 'venice_sunset_1k.hdr', texture => {
+
+                texture.mapping = THREE.EquirectangularReflectionMapping;
+
+                scene.background = texture;
+                scene.environment = texture;
+
+                render();
+            });
+    }
+
+    function createGUI(){
+        params = {
+            color: 0xffffff,
+            roughness: 0,
+            transmission: 1,
+            thickness: 2,
+        };
+        gui = new GUI();
+
+        gui.addColor( params, 'color' ).onChange( value => {
+            material.color.set( value );
+            render();
+        } );
+
+        gui.add( params, 'roughness', 0, 1 ).onChange( value => {
+            material.roughness = value;
+            render();
+        } );
+
+        gui.add( params, 'transmission', 0, 1 ).onChange( value => {
+            material.transmission = value;
+            render();
+        } );
+
+        gui.add( params, 'thickness', 0, 5 ).onChange( value => {
+            material.thickness = value;
+            render();
+        } );
+
+        render();
+
+    }
+
+    function loadModel(){
+        const loader = new GLTFLoader()
+            .setPath( '../../libs/three/examples/models/gltf/Nefertiti/' )
+            .load( 'Nefertiti.glb',  gltf => {
+
+                gltf.scene.scale.set( 0.1, 0.1, 0.1 );
+
+                material = new THREE.MeshPhysicalMaterial( {
+                    color: params.color,
+                    roughness: params.roughness,
+                    transmission: params.transmission,
+                    thickness: params.thickness,
+                } );
+
+                gltf.scene.traverse( child => {
+                    if (child.isMesh){
+                        child.material.dispose();
+                        child.material = material;
+                    }
+                });
+                    
+                scene.add( gltf.scene );    
+                
+                render();
+
+            } );
+    }
+
+    function render() {
+
+        renderer.render( scene, camera );
+
+    } 
---
+++ code-along/materials/toon.js
@@ -0,0 +1,105 @@
+import * as THREE from "three";
+    import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+    import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+    import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+    import { OutlineEffect } from 'three/addons/effects/OutlineEffect.js';
+    import { LoadingBar } from "../../libs/LoadingBar.js";
+
+    let camera, scene, renderer, clock, light, knight, mixer, outline;
+
+    init();
+    
+    function init() {
+        renderer = new THREE.WebGLRenderer({antialias: true, stencil: false});
+        renderer.setPixelRatio( Math.min(2, window.devicePixelRatio ));
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        renderer.outputColorSpace = THREE.SRGBColorSpace;
+        
+        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 50 );
+        camera.position.set( 1, 0.8, 2.5 );
+        camera.lookAt(0,0.8,0);
+        
+        const col = 0xAAAAAA;
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color( col );
+
+        clock = new THREE.Clock();
+
+        const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.2);
+		scene.add(ambient);
+
+        light = new THREE.DirectionalLight( 0xFFFFFF, 1 );
+        light.position.set( 4, 20, 20);
+        scene.add( light );
+
+        window.addEventListener( 'resize', onWindowResize, false );
+        
+        const controls = new OrbitControls( camera, renderer.domElement );
+        controls.target.set( 0, 0.8, 0 );
+        controls.update();
+        
+        loadModel();
+    }
+
+    function loadModel(){
+        const loader = new GLTFLoader( ).setPath('../../assets/');
+        const dracoLoader = new DRACOLoader();
+        dracoLoader.setDecoderPath( '../../libs/three/examples/jsm/libs/draco/' );
+        loader.setDRACOLoader( dracoLoader );
+                
+        const loadingBar = new LoadingBar();
+        loadingBar.visible = true;
+            
+        // Load a glTF resource
+        loader.load(
+            // resource URL
+            'knight3.glb',
+            // called when the resource is loaded
+            gltf => {
+                let material;
+
+                knight = gltf.scene;
+                knight.children[1].visible = false;
+
+                mixer = new THREE.AnimationMixer( knight );
+                const action = mixer.clipAction( gltf.animations[2] );
+                if ( action ) action.play();
+
+                //Add material replacement code here
+            
+                loadingBar.visible = false;
+            
+                scene.add( knight );
+            
+                render(); 
+            },
+            // called while loading is progressing
+            xhr => {
+                loadingBar.update(name, xhr.loaded, xhr.total);        
+            },
+            // called when loading has errors
+            err => {
+
+                console.error( err );
+
+            }
+        );
+    }
+
+    function onWindowResize() {
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+    }
+
+    function render() {
+        requestAnimationFrame( render );
+        const dt = clock.getDelta();
+        if (mixer) mixer.update(dt);
+
+        renderer.render( scene, camera );
+    } 
