fee497a87fd326e5bc845147a1cff79c88ef81e4
Nicholas Lever <nik@catalystpics.co.uk>
2025-08-27 10:37:57 +0100

    Shadows

---
+++ code-along/lighting/shadows.js
@@ -0,0 +1,215 @@
+import * as THREE from 'three';
+//import { OrbitControls } from "three/addons/controls/OrbitControls.js"
+//import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+
+let scene, camera, renderer, clock, light, lightOffset, helper, player, cameras, cameraIndex;
+
+init();
+
+function init() {
+
+	//const assetPath = '../../assets/';
+
+	clock = new THREE.Clock();
+
+	scene = new THREE.Scene();
+	const col = 0x807070;
+	scene.background = new THREE.Color( col );
+	scene.fog = new THREE.Fog( col, 10, 100 );
+
+	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	camera.position.set( 0, 4, 7 );//wide position
+	camera.lookAt( 0, 1.5, 0 );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.VSMShadowMap;
+	document.body.appendChild( renderer.domElement );
+
+	const ambient = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1.25 );
+	scene.add( ambient );
+
+	light = new THREE.DirectionalLight( 0xFFFFFF, 3 );
+	light.position.set( 1, 10, - 6 );
+	light.castShadow = true;
+	light.shadow.radius = 7;
+	light.shadow.blurSamples = 8;
+
+	scene.add( light );
+
+	//1. Add helper here
+
+
+	const planeGeometry = new THREE.PlaneGeometry( 200, 200 );
+	const planeMaterial = new THREE.MeshStandardMaterial( { color: 0x888888 } );
+	planeGeometry.rotateX( - Math.PI / 2 );
+	const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+	plane.receiveShadow = true;
+	scene.add( plane );
+
+	const grid = new THREE.GridHelper( 200, 80, 0x888888, 0xdddddd );
+	scene.add( grid );
+
+	//Add Playermeshes here
+	player = new THREE.Group();
+	scene.add( player );
+
+	//2. Targeting
+
+	const bodyGeometry = new THREE.CylinderGeometry( 0.5, 0.3, 1.6, 20 );
+	const material = new THREE.MeshStandardMaterial( { color: 0xffff00 } );
+	const body = new THREE.Mesh( bodyGeometry, material );
+	body.position.y = 0.8;
+	body.scale.z = 0.5;
+	body.castShadow = true;
+	player.add( body );
+	const headGeometry = new THREE.SphereGeometry( 0.3, 20, 15 );
+	const head = new THREE.Mesh( headGeometry, material );
+	head.position.y = 2.0;
+	head.castShadow = true;
+	player.add( head );
+
+	cameras = [];
+	cameraIndex = 0;
+
+	const followCam = new THREE.Object3D();
+	followCam.position.copy( camera.position );
+	player.add( followCam );
+	cameras.push( followCam );
+
+	const frontCam = new THREE.Object3D();
+	frontCam.position.set( 0, 3, - 15 );
+	player.add( frontCam );
+	cameras.push( frontCam );
+
+	const overheadCam = new THREE.Object3D();
+	overheadCam.position.set( 0, 20, 0 );
+	cameras.push( overheadCam );
+
+	addKeyboardControl();
+
+	const btn = document.getElementById( 'camera-btn' );
+	btn.addEventListener( 'click', changeCamera );
+
+	window.addEventListener( 'resize', resize, false );
+
+	update();
+
+}
+
+function changeCamera() {
+
+	cameraIndex ++;
+	if ( cameraIndex >= cameras.length ) cameraIndex = 0;
+
+}
+
+function addKeyboardControl() {
+
+	document.addEventListener( 'keydown', keyDown );
+	document.addEventListener( 'keyup', keyUp );
+
+}
+
+function keyDown( evt ) {
+
+	let forward = ( player.userData !== undefined && player.userData.move !== undefined ) ? player.userData.move.forward : 0;
+	let turn = ( player.userData != undefined && player.userData.move !== undefined ) ? player.userData.move.turn : 0;
+
+	switch ( evt.keyCode ) {
+
+		case 87://W
+			forward = - 1;
+			break;
+		case 83://S
+			forward = 1;
+			break;
+		case 65://A
+			turn = 1;
+			break;
+		case 68://D
+			turn = - 1;
+			break;
+
+	}
+
+	playerControl( forward, turn );
+
+}
+
+function keyUp( evt ) {
+
+	let forward = ( player.userData !== undefined && player.userData.move !== undefined ) ? player.userData.move.forward : 0;
+	let turn = ( player.move != undefined && player.userData.move !== undefined ) ? player.userData.move.turn : 0;
+
+	switch ( evt.keyCode ) {
+
+		case 87://W
+			forward = 0;
+			break;
+		case 83://S
+			forward = 0;
+			break;
+		case 65://A
+			turn = 0;
+			break;
+		case 68://D
+			turn = 0;
+			break;
+
+	}
+
+	playerControl( forward, turn );
+
+}
+
+function playerControl( forward, turn ) {
+
+	if ( forward == 0 && turn == 0 ) {
+
+		delete player.userData.move;
+
+	} else {
+
+		if ( player.userData === undefined ) player.userData = {};
+		player.userData.move = { forward, turn };
+
+	}
+
+}
+
+function update() {
+
+	const dt = clock.getDelta();
+	// eslint-disable-next-line compat/compat
+	requestAnimationFrame( update );
+	if ( player.userData !== undefined && player.userData.move !== undefined ) {
+
+		player.translateZ( player.userData.move.forward * dt * 5 );
+		player.rotateY( player.userData.move.turn * dt );
+		//3. Position the light
+
+	}
+
+	if ( helper !== undefined ) helper.update();
+
+	const target = cameras[ cameraIndex ].getWorldPosition( new THREE.Vector3() );
+	camera.position.lerp( target, 0.05 );
+
+	const pos = player.position.clone();
+	pos.y += 3;
+
+	if ( camera.position.distanceTo( target ) > 0.1 || cameraIndex < 2 ) camera.lookAt( pos );
+
+	renderer.render( scene, camera );
+
+}
+
+function resize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
