21da52116c084f254d8a46800dafc5649c683617
Nicholas Lever <nik@catalystpics.co.uk>
2025-09-22 12:24:02 +0100

    Cubecam update

--- demos/cubecam.js
+++ demos/cubecam.js
@@ -1,13 +1,14 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 // eslint-disable-next-line no-unused-vars
-let scene, camera, renderer, controls, object;
+let scene, camera, renderer, controls, object, clock;
 
 init();
 
 function init() {
+
+	clock = new THREE.Clock();
 
   	scene = new THREE.Scene();
   	scene.background = new THREE.Color( 0xaaaaaa );
@@ -41,23 +42,25 @@ function createCube( ) {
   	object = new THREE.Group();
 
 	const configs = [
-		{ x: - 0.5, y: 0, z: 0, h: Math.PI / 2, p: 0, b: 0, map: 'nx' },
-		{ x: 0.5, y: 0, z: 0, h: - Math.PI / 2, p: 0, b: 0, map: 'px' },
-		{ x: 0, y: 0, z: - 0.5, h: Math.PI, p: 0, b: 0, map: 'nz' },
-		{ x: 0, y: 0, z: 0.5, h: 0, p: 0, b: 0, map: 'pz' },
-		{ x: 0, y: - 0.5, z: 0, h: 0, p: - Math.PI / 2, b: 0, map: 'ny' },
-		{ x: - 0.5, y: 0, z: 0, h: 0, p: Math.PI / 2, b: 0, map: 'py' }
+		{ x: - 0.5, y: 0, z: 0, h: - Math.PI / 2, p: 0, b: 0, map: 'nx', axis: 'x', visible: true },
+		{ x: 0.5, y: 0, z: 0, h: Math.PI / 2, p: 0, b: 0, map: 'px', axis: 'x', visible: true },
+		{ x: 0, y: 0, z: - 0.5, h: Math.PI, p: 0, b: 0, map: 'nz', axis: 'z', visible: true },
+		{ x: 0, y: 0, z: 0.5, h: 0, p: 0, b: 0, map: 'pz', axis: 'z', visible: true },
+		{ x: 0, y: - 0.5, z: 0, h: 0, p: Math.PI / 2, b: 0, map: 'ny', axis: 'y', visible: true },
+		{ x: 0, y: 0.5, z: 0, h: 0, p: - Math.PI / 2, b: 0, map: 'py', axis: 'y', visible: true }
 	];
 
 	configs.forEach( config => {
 
 	    const quad = new THREE.PlaneGeometry();
 	    const material = new THREE.MeshBasicMaterial( { color: 0xFFFFFF } );
-		material.map = new THREE.TextureLoader().load( `../assets/skybox3/${config.map}.jpg` );
+		material.map = new THREE.TextureLoader().load( `../assets/skybox1/${config.map}.jpg` );
 		const mesh = new THREE.Mesh( quad, material );
 		mesh.position.set( config.x, config.y, config.z );
 		mesh.rotation.set( config.p, config.h, config.b );
-		object.add( mesh );
+		mesh.userData.axis = config.axis;
+		mesh.userData.start = config[ config.axis ];
+		if ( config.visible ) object.add( mesh );
 
 	} );
 
@@ -70,6 +73,12 @@ function update() {
   	// eslint-disable-next-line compat/compat
   	requestAnimationFrame( update );
 
+	const delta = 1 + ( Math.sin( clock.getElapsedTime() * 2 ) + 1 ) / 5;
+	object.children.forEach( child => {
+
+		child.position[ child.userData.axis ] = child.userData.start * delta;
+
+	} );
   	object.rotateY( 0.01 );
 
   	renderer.render( scene, camera );
